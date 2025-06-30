65a768e274f1a1dad99ea5f987c554358eeded40
Vladislav <winr2005@gmail.com>
2025-06-30 15:10:22 +0500

    Добавлен пробный маршрут react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import './App.module.css';
-import { App, ConfigProvider, Layout} from 'antd';
+import { App, ConfigProvider} from 'antd';
 import LayoutComponent from './pages/Layout/Layout';
 import { lightThemeConfig, darkThemeConfig } from './styles/theme';
 import Content from './components/Content/Content';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const AppComponent: React.FC = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -14,13 +14,17 @@ const AppComponent: React.FC = () => {
     localStorage.setItem('theme', (!isDarkMode).toString());
   };
   return (
+    <Router>
       <ConfigProvider theme={isDarkMode ? darkThemeConfig : lightThemeConfig}>
         <App> 
           <LayoutComponent isDarkMode={isDarkMode} toggleTheme={toggleTheme} >
-            <Content /> 
+          <Routes>
+            <Route path="/content" element={<Content />} />
+          </Routes> 
           </LayoutComponent>  
         </App>
       </ConfigProvider>
+    </Router>
 )};
 
 export default AppComponent;
