6eaf84f18ba93d044e41478c64e616750cd5c4ae
Vladislav <winr2005@gmail.com>
2025-07-09 15:49:25 +0500

    Backend: изменена подстановка env-переменных в конфиге

--- backend/src/auth/config.py
+++ backend/src/auth/config.py
@@ -12,5 +12,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 if not SECRET_KEY:
     raise ValueError("SECRET_KEY не найден в .env файле")
 
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ALGORITHM = os.getenv("ALGORITHM")
+if not ALGORITHM:
+    raise ValueError("SECRET_KEY не найден в .env файле")
+
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+if not ACCESS_TOKEN_EXPIRE_MINUTES:
+    raise ValueError("SECRET_KEY не найден в .env файле")
+
