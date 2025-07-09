d7d97694eff9a5a638336f682571b4f3f978e781
Vladislav <winr2005@gmail.com>
2025-07-09 11:04:17 +0500

    Backend: добавлена проверка на наличие набора в БД

--- backend/src/api/v1/deck_route.py
+++ backend/src/api/v1/deck_route.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from auth.schemas import UserInDBSchema
 from fastapi import APIRouter, Depends
 from auth.dependencies import get_current_active_user
@@ -25,4 +25,9 @@ async def get_decks_api(session: SessionDep):
 @router.get("/decks/{deck_id}/cards")
 async def get_deck_by_id_api(deck_id: int, session: SessionDep, current_user: UserInDBSchema = Depends(get_current_active_user)):
     result = await get_deck_by_id(deck_id=deck_id, session=session)
+    if not result:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Deck not found"
+        )
     return result
