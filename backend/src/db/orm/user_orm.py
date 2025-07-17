440a0865cb1a1f5bc61d79303fc4794a73980868
Vladislav <winr2005@gmail.com>
2025-07-17 11:39:18 +0500

    Backend: добавлено ограничение на регистрацию пользователей с одинаковым username или email

--- backend/src/db/orm/user_orm.py
+++ backend/src/db/orm/user_orm.py
@@ -1,4 +1,6 @@
 # Файл с описанием функций (методов) для создания запросов и команд базе данных
+from fastapi import HTTPException, status
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 from db.models.user_model import userModel
 from db.schemas.user_schemas import UserAddSchema
@@ -6,6 +8,24 @@ from auth.utils import get_password_hash
 
 
 async def add_user(data: UserAddSchema, session: Session):
+    existing_user = await session.execute(
+        select(userModel).where(userModel.username == data.username)
+    )
+    if existing_user.scalar():
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Пользователь с таким именем уже существует"
+        )
+    
+    existing_email = await session.execute(
+        select(userModel).where(userModel.email == data.email)
+    )
+    if existing_email.scalar():
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Пользователь с такой почтой уже существует"
+        )
+    
     new_user = userModel(
         username = data.username,
         email = data.email,
