0d8a1311069972f7028b60bc5288ba510b9bd129
Vladislav <winr2005@gmail.com>
2025-07-17 20:34:52 +0500

    Frontend: написана заглушка для нереализованного раздела настроек аккаунта пользователя

--- frontend/src/pages/Settings/Settings.tsx
+++ frontend/src/pages/Settings/Settings.tsx
@@ -1,7 +1,22 @@
+import { Button } from "antd";
+import styles from './Settings.module.css'
+import { useNavigate } from 'react-router-dom';
 
 
 const Settings: React.FC = () => {
-    return( null );
+    const navigate = useNavigate();
+    return(
+        <div className={styles.warningContainer}>
+          <h1>Раздел в разработке</h1>
+          <Button
+            type="primary" 
+            style={{ marginBottom: 16 }}
+            onClick={() => navigate('/main')}
+          >
+            На главную
+          </Button>
+        </div>  
+     );
 }
 
 export default Settings;
